183099dcff5a4fd988f90e478da1bc2c075388e2
Jidim <78730440+Jidim@users.noreply.github.com>
2022-10-21 01:30:39 +0300

    Update sudoku.hpp

--- app/sudoku.hpp
+++ app/sudoku.hpp
@@ -1,8 +1,6 @@
 #ifndef SUDOKU_HPP
 #define SUDOKU_HPP
 
-void player_turn(int(*board)[9]);
-
 void handler(int masking_cells);
 
 void gameover(int(*board)[9]);
@@ -18,8 +16,6 @@ void small_row_swipe(int j1, int j2, int(*board)[9]);
 void big_line_swipe(int b1, int b2, int(*board)[9]);
 
 void small_line_swipe(int i1, int i2, int(*board)[9]);
-
-void gameover(int(*board)[9]);
 
 bool block_error(int(*board)[9]);
 
