abe886525ecde70118214380313b8a8e1aae8ee2
Jidim <78730440+Jidim@users.noreply.github.com>
2022-10-21 04:31:53 +0300

    Update sudoku.cpp

--- app/sudoku.cpp
+++ app/sudoku.cpp
@@ -160,7 +160,7 @@ void hunter(int (*board)[9])
 {
     for (int i = 0; i < 9; i++)
         for (int j = 0; j < 9; j++)
-            if (board[i][j] != 0 && !ready)
+            if (board[i][j] != 0)
             {
                 board[i][j] = 0;
                 return;
